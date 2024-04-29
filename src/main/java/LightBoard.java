d3f162e4a4d8822d5504182812d16a512302af62
stephelociraptor <139821295+stephelociraptor@users.noreply.github.com>
2024-04-29 10:03:22 -0700

    Update LightBoard.java

--- src/main/java/LightBoard.java
+++ src/main/java/LightBoard.java
@@ -11,7 +11,7 @@ public class LightBoard
   public LightBoard(int numRows, int numCols)
   {
     /* to be implemented in part (a) */
-    lights = new boolean [numRows][numCols]
+    lights = new boolean [numRows][numCols];
     for (int r = 0; r < numRows; r++){
       for (int c = 0; c < numCols; c++){
         double random = Math.random();
